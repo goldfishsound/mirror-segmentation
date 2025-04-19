de9b963ebc5506367d927d49ae2fdd15a2c7e8e1
Thomas Martin <thomas@goldfishsound.com>
2025-04-19 16:04:05 +0200

    Added paths for logs and converted model

--- config.py
+++ config.py
@@ -3,12 +3,14 @@
 # Authors: Mark Edward M. Gonzales & Lorene C. Uy
 # ===============================================================================
 
+log_path = "logs/"
+
 testing_path = "dataset/test"
 training_path = "dataset/train"
 
 weights_path = "weights/efficientnet.pth"
 pruned_weights_path = "weights/sparsity-10.pth"
-
+converted_weights_path = "weights/converted_weights.pth"
 checkpoint_path = "ckpt"
 result_path = "result"
 
